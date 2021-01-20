893ced50c9e9ab31bc9969f6a6244cb6254e5f92
Mikhail Sakhniuk <sakhnyukmikhail@gmail.com>
2021-01-20 09:01:54 +0700

    fix: update linkedin link

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,7 +1,7 @@
 export const links = {
   github: 'https://github.com/sakhnyuk',
   twitter: 'https://twitter.com/MikhailSakhnyuk',
-  linkedin: 'https://www.linkedin.com/in/mikhail-sakhniuk-96765aa8/',
+  linkedin: 'https://www.linkedin.com/in/michael-sakhniuk-96765aa8/',
   email: 'mailto:sakhnyukmikhail@gmail.com',
 };
 
